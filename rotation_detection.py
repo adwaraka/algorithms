708c15d559ef21c2b53d362177b1962024adf265
Arvind D <arvindd25@gmail.com>
2017-10-26 11:55:59 -0700

    removed the print statements

--- rotation_detection.py
+++ rotation_detection.py
@@ -17,13 +17,13 @@ def rotation_search(arr, low, high):
         return -1
 
 
-arr = ["alexander", "ethan", "fred", "matt", "obara", "yaxley", "arvind", "bob"]
+arr = ["alexander", "ethan", "fred", "matt", "obara", "yaxley", "zulu", "arvind", "bob"]
 alpha_num_arr = []
 for i in arr:
     i = list(i)
     first_alpha_number = ord(i[0]) - ord('a')
     alpha_num_arr.append(first_alpha_number)
 
-print alpha_num_arr
+#print alpha_num_arr
 val = rotation_search(alpha_num_arr, 0, len(alpha_num_arr)-1)
 print val
