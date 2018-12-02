8412b77d55ca6be8a749784be9410f41fdd61d2f
Arvind D <arvindd25@gmail.com>
2018-12-01 18:34:31 -0800

    Some formatting

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,9 +3,9 @@
 #0 2 4 5 9
 def bubble_sort(ar):
     for j in reversed(xrange(0,len(ar))):
-		for i in xrange(j):
-			if (ar[i] > ar[i+1]):
-				ar[i],ar[i+1] = ar[i+1],ar[i]
+        for i in xrange(j):
+            if (ar[i] > ar[i+1]):
+                ar[i],ar[i+1] = ar[i+1],ar[i]
     for i in ar:
         print i,
 
